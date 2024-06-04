a7b3f7cfa530c8f8bc0975a7b5c7e79e96967ab3
za7499 <yehchenchen93@gmail.com>
2024-06-04 11:58:07 +0800

    角色移動
    假裝blue1.gif是角色
    目前初始位置在右下角
    可以跟隨上下左右鍵移動

--- player.java
+++ player.java
@@ -7,33 +7,40 @@ import java.awt.event.KeyListener;
 
 //目前只寫到了讓主程式可以讀取這個player程式碼的鍵盤事件
 public class player extends JPanel implements KeyListener{
-    private JLabel label;
+    private ImageIcon player_img;
+    private int x,y;
+    private int Map_y=550;//不知道大小是多少.....
     public player(){
-        
-        label = new JLabel("");
-        add(label);
+        player_img = new ImageIcon("blue1.gif");
         setFocusable(true);
         addKeyListener(this);
-        
 
+        //初始位子:右下
+        x=5;
+        y=Map_y;
+        System.out.println(y);
+       
+    }
+    public void paint(Graphics g) {
+        super.paintComponent(g);
+        Insets ins = getInsets(); // 取得邊線尺寸
+        // 繪出ImageIcon
+        player_img.paintIcon(this, g, x + ins.left, y + ins.top);
     }
     public void keyPressed(KeyEvent e){
         if(e.getKeyCode()==KeyEvent.VK_UP){//按鍵盤向上
-            label.setText("u");
-            
+            y-=5;
         }
         else if(e.getKeyCode()==KeyEvent.VK_DOWN){//下面
-            label.setText("d");
-            
+            y+=5;
         }
         else if(e.getKeyCode()==KeyEvent.VK_LEFT){//左邊
-            label.setText("l");
-            
+            x-=5;
         }
         else if(e.getKeyCode()==KeyEvent.VK_RIGHT){//右邊
-            label.setText("r");
-            
+            x+=5;
         }
+        repaint();//重畫
 
     }
     public void keyReleased(KeyEvent e){
