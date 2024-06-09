0b00b4be779dd82785aa3e56aae178c219cef25e
za7499 <yehchenchen93@gmail.com>
2024-06-09 19:21:41 +0800

    寶箱
    也是寫在player.java中
    目前是的機制是:可以獲得金幣(1~20)或是有機會開出寶相怪(及遊戲Game over)
    
    為了到時demo，有設置一個開出來絕對是寶箱怪的的寶箱map(x=2,y=1)

--- player.java
+++ player.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 public class player extends JPanel {
     private ImageIcon player_img;
@@ -23,12 +24,15 @@ public class player extends JPanel {
     private BufferedImage roadImage;
     private BufferedImage wallImage;
     private BufferedImage doorImage;
+    private BufferedImage treasureIMage;
+    private BufferedImage treasureOpenIMage;
 
 
     // 地圖//只是隨便加一個地圖試試
+    //0:路  1:牆  2:門  3:寶箱
     private int[][] map = {
         {0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2},
-        {0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0},
+        {0, 1, 3, 0, 0, 3, 1, 0, 1, 1, 0},
         {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0},
@@ -124,6 +128,10 @@ public class player extends JPanel {
             roadImage = ImageIO.read(new File("road_0000.png"));
             wallImage = ImageIO.read(new File("wall_0014.png"));
             doorImage = ImageIO.read(new File("door_0045.png"));
+            treasureIMage = ImageIO.read(new File("treasure_0089.png"));
+            treasureOpenIMage = ImageIO.read(new File("treasureopen_0091.png"));
+            
+            
             
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -146,12 +154,83 @@ public class player extends JPanel {
                 gamePanel.gameWin();
             }
 
+            //寶箱觸發事件
+            if (map[y][x] == 3) {
+                treasure_event();
+                map[y][x] = 4;//標記寶箱為開過
+            }
+
             System.out.println("x:" + x + " y:" + y); // 玩家位置
             repaint();
             checkMonsterProximity();
         }
     }
 
+    //寶箱事件
+    private void treasure_event(){
+        Random rand = new Random();
+        int outcome = rand.nextInt(10); // 隨機生成0~9
+        if(x==2&& y==1)outcome=0;//為了demo時的寶箱怪設置的
+
+        if (outcome == 0) {
+            // 10%的概率出現寶箱怪
+            showGameOverAnimation();//寶箱怪小動畫和Gameover提示
+        } else {
+            // 獲得1~20元
+            int coins = rand.nextInt(20) + 1;
+            JOptionPane.showMessageDialog(this, "你獲得了 " + coins + " 個金幣！");
+        }
+    }
+    //寶箱怪動畫-----------------------------------------------------------
+    private void showGameOverAnimation() {
+        JDialog dialog = new JDialog((Frame) null, "遊戲結束", true);
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        dialog.setLayout(new BorderLayout());
+        JLabel msg=new JLabel("好黑~好可怕\n這裡面是珍貴的魔導書(才怪");
+
+        JPanel animationPanel = new JPanel() {
+            private ImageIcon[] icons = {
+                new ImageIcon("treasure_0089.png"),
+                new ImageIcon("treasure_0090.png"),
+                new ImageIcon("treasureopen_0091.png"),
+                new ImageIcon("treasure_0092.png")
+            };
+            private int currentFrame = 0;
+
+            {
+                Timer timer = new Timer(200, new AbstractAction() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        currentFrame = (currentFrame + 1) % icons.length;
+                        repaint();
+                    }
+                });
+                timer.start();
+            }
+
+            @Override
+            protected void paintComponent(Graphics g) {
+                Image img = icons[currentFrame].getImage();
+                g.drawImage(img, 80, 30, 50, 50, this);
+                
+            }
+
+            @Override
+            public Dimension getPreferredSize() {
+                return new Dimension(200, 100);
+            }
+        };
+        
+        dialog.add(animationPanel, BorderLayout.WEST);
+        dialog.add(msg,BorderLayout.SOUTH);
+        dialog.pack();
+        dialog.setLocationRelativeTo(this);
+        dialog.setVisible(true);
+
+        gamePanel.gameOver(); // 调用gameOver方法结束游戏
+    }
+
+    //----------------------------------------
     private void updatePosition() {
         // 確保玩家在範圍內
         //x = Math.max(0, Math.min(x, panelWidth - player_img.getIconWidth()));
@@ -162,7 +241,7 @@ public class player extends JPanel {
         checkMonsterProximity();
     }
 
-    //畫玩家和怪獸和地圖---------------------------------------------------
+    //畫玩家和怪獸和地圖和寶箱---------------------------------------------------
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -174,8 +253,12 @@ public class player extends JPanel {
                 BufferedImage img;
                 if (map[i][j] == 1) {
                     img = wallImage;
-                } else if (map[i][j] == 2) {
+                } else if (map[i][j] == 2) {//門
                     img=doorImage;
+                } else if (map[i][j] == 3) {//還沒開過的寶箱
+                    img=treasureIMage;
+                }else if (map[i][j] == 4) {//開過的寶箱
+                    img=treasureOpenIMage;
                 } else {
                     img = roadImage;
                 }
